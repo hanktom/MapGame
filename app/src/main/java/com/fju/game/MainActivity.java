f62ae7b0d9f10e48797158d46868e88386cb6c04
Hank Tom <hanktom@gmail.com>
2019-12-17 15:20:34 +0800

    switch view.id

--- app/src/main/java/com/fju/game/MainActivity.java
+++ app/src/main/java/com/fju/game/MainActivity.java
@@ -13,35 +13,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button bLeft = findViewById(R.id.button_left);
-        bLeft.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        findViews();
+    }
 
-            }
-        });
-
+    private void findViews() {
         findViewById(R.id.button_left).setOnClickListener(this);
         findViewById(R.id.button_right).setOnClickListener(this);
         findViewById(R.id.button_up).setOnClickListener(this);
         findViewById(R.id.button_down).setOnClickListener(this);
     }
 
-    public void left(View view) {
-
-    }
-    public void right(View view) {
-
-    }
-    public void up(View view) {
-
-    }
-    public void down(View view) {
-
-    }
-
     @Override
-    public void onClick(View v) {
-        Log.d("MainActivity", "onClick");
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_up:
+                Log.d("MainActivity", "onClick: UP");
+                break;
+            case R.id.button_down:
+                Log.d("MainActivity", "onClick: DOWN");
+                break;
+            case R.id.button_left:
+                Log.d("MainActivity", "onClick: LEFT");
+                break;
+            case R.id.button_right:
+                Log.d("MainActivity", "onClick: RIGHT");
+                break;
+        }
+
     }
 }
